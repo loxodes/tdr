56c03973f286130c517a6bfebb43fd9cc6e4d139
Jon Klein <jtklein@alaska.edu>
2019-02-27 20:40:39 -0700

    fix crossover distortion on analog triggered measurements

--- software/plot.py
+++ software/plot.py
@@ -3,10 +3,10 @@ from pylab import *
 
 f = file('sweepfile', 'rb')
 sweep = np.load(f)
-t = np.arange(512) * 11.2e-12
-v = (sweep - 512.) * (4 / 1024.)
+t = np.arange(len(sweep)) * 11.2e-12
+v = (sweep - 2048.) * (4 / 4096.)
 plt.plot(t * 1e9, v)
-plt.title('falling edge of TDR pulse, w/ 3 dB attenuator')
+#plt.title('falling edge of TDR pulse, w/ 3 dB attenuator')
 plt.ylabel('voltage')
 plt.xlabel('time (ns)')
 plt.savefig('sweep.png')
